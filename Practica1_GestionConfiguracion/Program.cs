44e69cc8dc66ae365b8bcca81fc18f7fd30139a8
Railyng_04 <Railyng_04@RAILYNG_04>
2025-11-10 23:39:17 -0400

    Practica1_GestionConfiguracion 2.0

--- Practica1_GestionConfiguracion/Program.cs
+++ Practica1_GestionConfiguracion/Program.cs
@@ -13,6 +13,7 @@ namespace Practica1_GestionConfiguracion
 
             Console.WriteLine("\nPresiona cualquier tecla para salir...");
             Console.ReadKey();
+            Console.WriteLine("¡Práctica completada con éxito! 🚀");
         }
     }
 }
